2114949923446e2f896337314bc0e8b21461fd3c
maxrt <max.r.tkachuk@gmail.com>
2025-10-08 06:12:52 +0300

    Remove STM32L051 board, as cortex m0+ doesn't support PIC

--- boards/STM32F100/options.cmake
+++ boards/STM32F100/options.cmake
@@ -8,8 +8,14 @@ project_add_compile_options(ALL
         -masm-syntax-unified
 )
 
+project_add_compile_options(ALL
+    -Os
+)
 
 project_add_link_options(ALL
         -mcpu=cortex-m3
         -specs=nano.specs
+        -ffunction-sections -fdata-sections
+        -Wl,--gc-sections,--sort-common
+        -Wl,--start-group -lc -lm -Wl,--end-group
 )
